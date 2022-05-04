b0f35c9889cd955187701dd44285279736ddebf7
luu quang hao <luuquanghao2412@gmail.com>
2022-05-04 21:56:36 +0700

    feat: estimate veASH for BoY

--- context/gov/index.tsx
+++ context/gov/index.tsx
@@ -22,6 +22,7 @@ import {
 import BigNumber from "bignumber.js";
 import { ASHSWAP_CONFIG } from "const/ashswapConfig";
 import { blockTimeMs, gasLimit } from "const/dappConfig";
+import { ENVIRONMENT } from "const/env";
 import pools from "const/pool";
 import { ASH_TOKEN } from "const/tokens";
 import useContracts from "context/contracts";
@@ -39,6 +40,12 @@ import {
     useState,
 } from "react";
 const estimateVeASH = (weiAmt: BigNumber, lockSeconds: number) => {
+
+    if(ENVIRONMENT.NETWORK === "devnet"){
+        // ratio: lock 1 ASH in 2 weeks(14 days) -> 1 veASH
+        const veASHPerS = toEGLDD(ASH_TOKEN.decimals, weiAmt).multipliedBy(new BigNumber(1).div(2 * 7 * 24 * 60 * 60));
+        return toWei(ASH_TOKEN, veASHPerS.multipliedBy(lockSeconds).toString());
+    }
     // ratio: lock 1 ASH in 1 year(365 days) -> 0.25 veASH
     const veASHPerSecond = toEGLDD(ASH_TOKEN.decimals, weiAmt).multipliedBy(
         new BigNumber(0.25).div(365 * 24 * 60 * 60)
