3939a6cb59727f7bd2ec5ad0ee4b45688beeca7e
Royce <hieutrunglephan@gmail.com>
2022-05-28 07:37:06 +0700

    fix: update testnet addresses (#109)

--- const/maiarPools.ts
+++ const/maiarPools.ts
@@ -3,7 +3,9 @@ import IconUSDC from "assets/images/usdc-icon.png";
 import IconUSDT from "assets/images/usdt-icon.png";
 import IconWBTC from "assets/images/wbtc-icon.png";
 import IPool from "interface/pool";
-export const MAIAR_POOLS: IPool[] = [
+import { ENVIRONMENT } from "./env";
+
+export const devnet: IPool[] = [
     {
         address:
             "erd1qqqqqqqqqqqqqpgq9efzwnujjm6f75pccujr2jp4j5csym0vrmcqg9r3ff",
@@ -55,3 +57,58 @@ export const MAIAR_POOLS: IPool[] = [
         isMaiarPool: true
     },
 ];
+
+export const testnet: IPool[] = [
+    {
+        address:
+            "erd1qqqqqqqqqqqqqpgqwsmx0d2x20v855ljrh55swg9q4tr06y5j9ts38pupx",
+        lpToken: {
+            id: "LPT-d8005f",
+            icon: "#fff",
+            name: "LPT-d8005f",
+            decimals: 18,
+        },
+        tokens: [
+            {
+                id: "ASH-f01858",
+                icon: IconAsh,
+                name: "ASH",
+                decimals: 18,
+            },
+            {
+                id: "USDT-8d1668",
+                icon: IconUSDT,
+                name: "USDT",
+                decimals: 6,
+            },
+        ],
+        isMaiarPool: true,
+    },
+    {
+        address:
+            "erd1qqqqqqqqqqqqqpgqvv2p4u35v5yayn0s6ksf8ug97uf64dduj9tskx0eve",
+        lpToken: {
+            id: "LPT-8b0f7b",
+            icon: "#fff",
+            name: "LPT-8b0f7b",
+            decimals: 18,
+        },
+        tokens: [
+            {
+                id: "WBTC-ebec12",
+                icon: IconWBTC,
+                name: "wBTC",
+                decimals: 8,
+            },
+            {
+                id: "USDC-cbf0b9",
+                icon: IconUSDC,
+                name: "USDC",
+                decimals: 6,
+            },
+        ],
+        isMaiarPool: true
+    },
+];
+
+export const MAIAR_POOLS = ENVIRONMENT.NETWORK == "devnet" ? devnet : testnet;
