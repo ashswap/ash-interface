4864e2dd49102e2b185271e9c8b24c6d7258d43a
haoquang24 <97107463+haoquang24@users.noreply.github.com>
2022-10-13 15:54:18 +0700

    feat: reduce delay update ash base state after tx completed (#165)

--- atoms/ashswap.ts
+++ atoms/ashswap.ts
@@ -1,5 +1,6 @@
 import { AshBaseState } from "graphql/type";
 import { atom } from "recoil";
+import { KeyedMutator } from "swr";
 
 export const ashswapBaseState = atom<AshBaseState>({
     key: "ashswap_base_state",
@@ -12,4 +13,11 @@ export const ashswapBaseState = atom<AshBaseState>({
         blockchain: {},
         ashSupply: "0"
     }
+});
+
+export const ashBaseStateRefresherAtom = atom<
+    KeyedMutator<any>
+>({
+    key: "refresh_ash_base_state",
+    default: () => Promise.resolve(undefined),
 });
