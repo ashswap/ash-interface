a71d19eb7a285250b8d9edfe99f29cbe9503462f
Otis <otis@Otiss-MacBook-Air.local>
2022-12-08 23:28:53 +0700

    correct env name

--- const/env.ts
+++ const/env.ts
@@ -22,5 +22,5 @@ export const ENVIRONMENT: Environment = {
     ASH_SOCKET: process.env.NEXT_PUBLIC_ASH_SOCKET!,
     ASH_SOCKET_EXTRA: process.env.NEXT_PUBLIC_ASH_SOCKET_EXTRA!,
     ENV: (process.env.NEXT_PUBLIC_ASH_ENV as Env) || "beta",
-    LOGIN_DISCORD_LINK: process.env.NEXT_PUBLIC_LOGIN_DISCORD_LINK!
+    LOGIN_DISCORD_LINK: process.env.NEXT_PUBLIC_ASH_LOGIN_DISCORD_LINK!
 };
