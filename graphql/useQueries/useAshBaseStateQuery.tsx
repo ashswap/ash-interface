0f4cbfcac142eca01782922977a220ffd2c62048
otis <luuquanghao2412@gmail.com>
2023-01-09 22:41:19 +0700

    feat: update farm calculate rewards for position

--- graphql/useQueries/useAshBaseStateQuery.tsx
+++ graphql/useQueries/useAshBaseStateQuery.tsx
@@ -31,6 +31,7 @@ const useAshBaseStateQuery = (config?: SWRConfiguration) => {
                         lastRewardBlockNone
                         divisionSafetyConstant
                         farmingTokenBalance
+                        produceRewardEnabled
                         account {
                             slopeBoosted
                         }
