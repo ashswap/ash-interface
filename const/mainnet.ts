e30a9a7b4964438faeee666152df9eefa0dbafcd
Hieu Phan <hieutrunglephan@gmail.com>
2023-01-20 18:20:39 +0700

    update config

--- const/mainnet.ts
+++ const/mainnet.ts
@@ -3,7 +3,7 @@ import { TokenAmount } from "helper/token/tokenAmount";
 import { ASH_ESDT, VE_ASH_DECIMALS } from "./tokens";
 
 export const REWARD_DISTRIBUTOR_CONTRACT =
-    "erd1qqqqqqqqqqqqqpgqwfz3vxuwkpvnc9zh9xq4ur0cf5eg2m8lrmcqxwwtpr";
+    "erd1qqqqqqqqqqqqqpgqjfnu9jt6p4679nyw0xfwrqahfw6lje4xrmcqge6evj";
 export const LKASH_CONTRACT = "erd1qqqqqqqqqqqqqpgqxsyrdj38fclflak2mwtqdknqzqhuxy4s0n4sc3e0jy";
 export const LK_ASH_COLLECTION = "LKASH-261982";
 export const TOTAL_REWARD_POOL = new TokenAmount(
@@ -11,9 +11,9 @@ export const TOTAL_REWARD_POOL = new TokenAmount(
     new BigNumber(2_000_000).multipliedBy(10 ** ASH_ESDT.decimals)
 );
 export const LAUNCH_TS = 1676592000;
-export const START_REWARD_POOL = 1674313200;
+export const START_REWARD_POOL = 1674213600;
 export const END_REWWARD_POOL = 1676649600;
-export const UNLOCK_TS = 1674190205;// 1674432000;
+export const UNLOCK_TS = 1674214200;// 1674432000;
 export const REWARD_POOL_MIN_VE = new BigNumber(200).multipliedBy(
     10 ** VE_ASH_DECIMALS
 );
