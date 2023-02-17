b79cbe6670de708d554c9d5c0ed2e905a81a0d4e
otis <luuquanghao2412@gmail.com>
2023-02-18 00:20:17 +0700

    feat: disable ashpoint devnet

--- pages/ashpoint.tsx
+++ pages/ashpoint.tsx
@@ -1,7 +1,17 @@
 import BasicLayout from "components/Layout/Basic";
+import { ENVIRONMENT } from "const/env";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import QuestOverview from "views/quest/QuestOverview";
 
 function AshPoint() {
+    const router = useRouter();
+    useEffect(() => {
+        if (!ENVIRONMENT.ENABLE_ASHPOINT) {
+            router.replace("/swap");
+        }
+    }, [router]);
+    if(!ENVIRONMENT.ENABLE_ASHPOINT) return null;
     return (
         <BasicLayout>
             <div className="ash-container pb-40 pt-8">
