c27e9eb7791ab3ecdb98f6a6c950606043eb9564
otis <luuquanghao2412@gmail.com>
2023-02-18 22:17:53 +0700

    feat: enable swap page mainnet

--- pages/swap.tsx
+++ pages/swap.tsx
@@ -1,19 +1,9 @@
 import BasicLayout from "components/Layout/Basic";
-import { ENVIRONMENT } from "const/env";
 import { SwapProvider } from "context/swap";
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import Swap from "views/swap/Swap";
 
 const Home: NextPage = () => {
-
-    const router = useRouter();
-    useEffect(() => {
-        if (ENVIRONMENT.NETWORK === "mainnet" && router.route === "/swap") {
-            router.replace("/pool");
-        }
-    }, [router]);
     return (
         <BasicLayout>
             <SwapProvider>
