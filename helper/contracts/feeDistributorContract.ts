92942a0bd9fce58d2a5558c3e560ecfe63f9c45f
otis <luuquanghao2412@gmail.com>
2023-02-20 17:45:36 +0700

    fix: unsupport webwallet on ashpoint page

--- helper/contracts/feeDistributorContract.ts
+++ helper/contracts/feeDistributorContract.ts
@@ -1,6 +1,7 @@
 import Contract from "./contract";
 import feeDistributorAbi from "assets/abi/fee_distributor.abi.json";
 import { Address } from "@elrondnetwork/erdjs/out";
+import { ENVIRONMENT } from "const/env";
 
 class FeeDistributorContract extends Contract<typeof feeDistributorAbi> {
     constructor(address: string) {
@@ -9,7 +10,7 @@ class FeeDistributorContract extends Contract<typeof feeDistributorAbi> {
 
     async claim(address: Address) {
         let interaction = this.contract.methods.claim([address]);
-        interaction.withGasLimit(500_000_000);
+        interaction.withGasLimit(ENVIRONMENT.NETWORK === "mainnet" ? 50_000_000 : 500_000_000);
         return this.interceptInteraction(interaction)
             .check()
             .buildTransaction();
