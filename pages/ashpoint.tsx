7c1cfb239a0cb768d77d2bb59667f06050578adf
otis <luuquanghao2412@gmail.com>
2023-02-20 17:45:36 +0700

    fix: unsupport webwallet on ashpoint page

--- pages/ashpoint.tsx
+++ pages/ashpoint.tsx
@@ -1,17 +1,65 @@
+import { useGetLoginInfo } from "@elrondnetwork/dapp-core/hooks";
+import { LoginMethodsEnum } from "@elrondnetwork/dapp-core/types";
+import { logout } from "@elrondnetwork/dapp-core/utils";
+import ICWallet from "assets/svg/wallet.svg";
+import { accAddressState } from "atoms/dappState";
+import BaseModal, { BaseModalType } from "components/BaseModal";
 import BasicLayout from "components/Layout/Basic";
 import { ENVIRONMENT } from "const/env";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { memo, useEffect } from "react";
+import { useRecoilValue } from "recoil";
 import QuestOverview from "views/quest/QuestOverview";
-
+import Link from "next/link";
+const WebWalletInformModal = memo(function WebWalletInformModal(
+    props: Omit<BaseModalType, "children">
+) {
+    return (
+        <BaseModal
+            {...props}
+            type={"modal"}
+            className="clip-corner-4 clip-corner-tl bg-ash-dark-600 p-4
+                        max-w-md mx-auto
+                        relative inset-x-0 max-h-screen overflow-auto text-white flex flex-col"
+        >
+            <div className="grow overflow-auto px-4 py-10 flex flex-col items-center text-center">
+                <div className="mb-10 font-bold text-xs xs:text-sm text-white">
+                    Web wallet is temporarily unsupported for our ASH points
+                    program due to server maintenance. Please use other types of
+                    wallet for now.
+                </div>
+                <button
+                    className="mb-4 clip-corner-1 clip-corner-br bg-pink-600 text-white w-48 h-10 flex items-center justify-center"
+                    onClick={() => logout(window?.location?.href)}
+                >
+                    <ICWallet className="h-5 w-5 mr-2" />
+                    <span className="text-xs font-bold">Disconnect</span>
+                </button>
+                <Link href="/swap"><a><span className="text-stake-gray-500 text-xs">Back to swap</span></a></Link>
+            </div>
+        </BaseModal>
+    );
+});
 function AshPoint() {
     const router = useRouter();
+    const { loginMethod, isLoggedIn } = useGetLoginInfo();
+    const userAddress = useRecoilValue(accAddressState);
     useEffect(() => {
         if (!ENVIRONMENT.ENABLE_ASHPOINT) {
             router.replace("/swap");
         }
     }, [router]);
-    if(!ENVIRONMENT.ENABLE_ASHPOINT) return null;
+    useEffect(() => {
+        console.log(loginMethod, isLoggedIn);
+    }, [loginMethod, isLoggedIn]);
+    if (!ENVIRONMENT.ENABLE_ASHPOINT) return null;
+    if (loginMethod === LoginMethodsEnum.wallet)
+        return (
+            <WebWalletInformModal
+                isOpen={loginMethod === LoginMethodsEnum.wallet && isLoggedIn && !!userAddress}
+            />
+        );
+
     return (
         <BasicLayout>
             <div className="ash-container pb-40 pt-8">
