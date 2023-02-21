fb2ab542070d26b1547764f029e5f2a60b3f62ad
otis <luuquanghao2412@gmail.com>
2023-02-22 00:28:34 +0700

    feat: ashpoint require sign msg

--- atoms/ashpoint.ts
+++ atoms/ashpoint.ts
@@ -1,26 +1,52 @@
+import storage from "helper/storage";
 import { CustomQuestMapModel, QuestUserStatsModel } from "interface/quest";
 import { atom, selector } from "recoil";
 import { accAddressState } from "./dappState";
 
 export const atomQuestUserStats = atom<QuestUserStatsModel | undefined>({
-    key: 'quest_user_stats_atom',
-    default: undefined
+    key: "quest_user_stats_atom",
+    default: undefined,
 });
 
 export const atomCustomQuestData = atom<CustomQuestMapModel | undefined>({
     key: "custom_quest_data",
-    default: undefined
-})
+    default: undefined,
+});
 
-export const questIsRegisteredSelector = selector({
+export const questIsRegisteredAtom = atom<boolean>({
     key: "quest_is_registered",
+    default: false,
+});
+const questSignatureAtom = atom<{ signature?: string; rejected?: boolean }>({
+    key: "quest_signature_atom",
+    default: undefined,
+});
+export const questOwnerSignatureSelector = selector<{
+    signature?: string;
+    rejected?: boolean;
+}>({
+    key: "quest_owner_signature_selector",
     get: ({ get }) => {
-        const userStats = get(atomQuestUserStats);
-        const userAddress = get(accAddressState);
-        return (
-            (userStats?.wallet?.twitter_username ||
-                userStats?.wallet?.discord_id) &&
-            userStats.wallet.wallet_address === userAddress
-        );
-    }
+        const address = get(accAddressState);
+        const state = address
+            ? get(questSignatureAtom) ||
+              storage.local.getItem("ashpointOwners")?.[address] ||
+              {}
+            : {};
+        return state;
+    },
+    set: ({ set, get }, val) => {
+        const address = get(accAddressState);
+        const map = storage.local.getItem("ashpointOwners") || {};
+        storage.local.setItem({
+            key: "ashpointOwners",
+            data: { ...map, [address]: val },
+        });
+        set(questSignatureAtom, val);
+    },
+});
+
+export const questIsOpenOwnerSignModalAtom = atom({
+    key: "quest_is_open_owner_sign_modal_atom",
+    default: false,
 })
---
+++ hooks/useAshpoint.tsx
@@ -0,0 +1,80 @@
+import { useGetAccountProvider } from "@elrondnetwork/dapp-core/hooks";
+import { LoginMethodsEnum } from "@elrondnetwork/dapp-core/types";
+import { getAccountProviderType } from "@elrondnetwork/dapp-core/utils";
+import * as Sentry from "@sentry/nextjs";
+import {
+    atomQuestUserStats,
+    questIsOpenOwnerSignModalAtom,
+    questIsRegisteredAtom,
+    questOwnerSignatureSelector
+} from "atoms/ashpoint";
+import { accAddressState, accIsLoggedInState } from "atoms/dappState";
+import { ENVIRONMENT } from "const/env";
+import logApi from "helper/logHelper";
+import { QuestUserStatsModel } from "interface/quest";
+import { useRouter } from "next/router";
+import { useCallback, useEffect } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+
+const useAshpoint = () => {
+    const router = useRouter();
+    const accAddress = useRecoilValue(accAddressState);
+    const isLoggedIn = useRecoilValue(accIsLoggedInState);
+    const setIsRegistered = useSetRecoilState(questIsRegisteredAtom);
+    const setUserStats = useSetRecoilState(atomQuestUserStats);
+    const setIsOpenOwnerSign = useSetRecoilState(questIsOpenOwnerSignModalAtom);
+    const { providerType } = useGetAccountProvider();
+    const address = useRecoilValue(accAddressState);
+    const { signature: ashpointSignature, rejected: ashpointRejectedSign } =
+        useRecoilValue(questOwnerSignatureSelector);
+    const ashpointIsRegistered = useRecoilValue(questIsRegisteredAtom);
+
+    const getUserStats = useCallback(() => {
+        if (ENVIRONMENT.ENABLE_ASHPOINT && ashpointSignature) {
+            logApi
+                .get<QuestUserStatsModel>("/api/v1/wallet")
+                .then((res) => setUserStats(res.data))
+                .catch((err) => Sentry.captureException(err));
+        }
+    }, [ashpointSignature, setUserStats]);
+
+    useEffect(() => {
+        const provider = getAccountProviderType();
+        if (accAddress && isLoggedIn && provider !== LoginMethodsEnum.wallet) {
+            logApi
+                .get("/api/v1/no-auth/wallet", {
+                    params: { address: accAddress },
+                })
+                .then((data) => setIsRegistered(data.data.is_exist));
+        }
+    }, [accAddress, isLoggedIn, setIsRegistered]);
+
+    useEffect(() => getUserStats(), [getUserStats]);
+
+    useEffect(() => {
+        if (
+            providerType !== LoginMethodsEnum.wallet &&
+            address &&
+            !ashpointSignature
+        ) {
+            if (
+                router.pathname.startsWith("/ashpoint") ||
+                (ashpointIsRegistered && !ashpointRejectedSign)
+            ) {
+                setIsOpenOwnerSign(true);
+            }
+        } else {
+            setIsOpenOwnerSign(false);
+        }
+    }, [
+        providerType,
+        router.pathname,
+        address,
+        ashpointSignature,
+        ashpointIsRegistered,
+        ashpointRejectedSign,
+        setIsOpenOwnerSign,
+    ]);
+};
+
+export default useAshpoint;
