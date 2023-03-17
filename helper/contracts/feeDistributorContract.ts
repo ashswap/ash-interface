239f4f845d488c2a28ca7a3ed8bbaa3c601d50c4
royce <hieutrunglephan@gmail.com>
2023-03-17 16:44:47 +0000

    Merge branch 'hotfix/fd-gas' into 'main'
    Hotfix: fd gas
    
    See merge request ashswap/ash-interface!140
    
    (cherry picked from commit 9dd0e46ee09ad05e95c7a2ab1deaac24fe558ebd)
    
    d0abb0b6 hotfix: increase fee distributor gaslimit
    edc30efe hotfix: edit voting date format

--- helper/contracts/feeDistributorContract.ts
+++ helper/contracts/feeDistributorContract.ts
@@ -2,6 +2,7 @@ import Contract from "./contract";
 import feeDistributorAbi from "assets/abi/fee_distributor.abi.json";
 import { Address } from "@multiversx/sdk-core/out";
 import { ENVIRONMENT } from "const/env";
+import { WEEK } from "const/ve";
 
 class FeeDistributorContract extends Contract<typeof feeDistributorAbi> {
     constructor(address: string) {
@@ -9,14 +10,25 @@ class FeeDistributorContract extends Contract<typeof feeDistributorAbi> {
     }
 
     async claim(address: Address) {
+        const timeCursor = await this.getUserTimeCursor(address);
+        const iter = Math.ceil((Math.floor(Date.now() / 1000) - timeCursor) / WEEK);
+        const gas = timeCursor === 0 ? 500_000_000 : iter * 10_000_000;
         let interaction = this.contract.methods.claim([address]);
         interaction.withGasLimit(
-            ENVIRONMENT.NETWORK === "mainnet" ? 50_000_000 : 500_000_000
+            ENVIRONMENT.NETWORK === "mainnet" ? 50_000_000 + gas : 500_000_000
         );
         return this.interceptInteraction(interaction)
             .check()
             .buildTransaction();
     }
+
+    async getUserTimeCursor(address: Address): Promise<number> {
+        let interaction = this.contract.methods.getUserTimeCursor([address]);
+        const { firstValue } = await this.runQuery(interaction);
+        return firstValue?.valueOf()?.toNumber() || 0;
+    }
+
+
 }
 
 export default FeeDistributorContract;
