7273e1206fdb8e6e94ba5316126a85f3a556dd79
otis <luuquanghao2412@gmail.com>
2023-04-05 18:45:27 +0700

    fix: incorrect proposal type

--- const/proposal.ts
+++ const/proposal.ts
@@ -1,20 +1,23 @@
 import { ASHSWAP_CONFIG } from "./ashswapConfig";
 
-interface Config {
-    [key: `${keyof typeof PROPOSALS_UNALIAS}:${string}`]: string
-}
-export const PROPOSALS_CONFIG: Config = {
-    ["fc:addFarm"]: "Whitelist farm to receive ASH reward",
-    [`fr:endProduceRewards`]: "Stop ASH reward for a farm",
-    [`fr:startProduceRewards`]: "Start ASH reward for a farm",
+const _PROPOSALS_CONFIG = {
+    "fc:addFarm": "Whitelist farm to receive ASH reward",
+    "fr:endProduceRewards": "Stop ASH reward for a farm",
+    "fr:startProduceRewards": "Start ASH reward for a farm",
 } as const;
 
-export type ProposalType = keyof typeof PROPOSALS_CONFIG;
 export const PROPOSALS_UNALIAS = {
     fc: ASHSWAP_CONFIG.dappContract.farmController,
     fr: ASHSWAP_CONFIG.dappContract.farmRouter,
     fb: ASHSWAP_CONFIG.dappContract.farmBribe,
     pr: ASHSWAP_CONFIG.dappContract.router,
 } as const;
-
 export const PROPOSALS_ALIAS = Object.fromEntries(Object.entries(PROPOSALS_UNALIAS).map(([k, v]) => [v, k as keyof typeof PROPOSALS_UNALIAS]));
+export type ProposalTypePrefix = keyof typeof PROPOSALS_UNALIAS;
+export type ProposalType = keyof typeof _PROPOSALS_CONFIG;
+type Config = {[key in ProposalType]: string};
+interface GenericConfig {
+    [key: `${ProposalTypePrefix}:${string}`]: string;
+}
+export const PROPOSALS_CONFIG: GenericConfig & Config = _PROPOSALS_CONFIG;
+
