2912f810a835ff07c87e78e441d00a867b4154c1
otis <luuquanghao2412@gmail.com>
2023-06-01 11:58:27 +0700

    fix: select token in legacy swap

--- pages/legacy-swap.tsx
+++ pages/legacy-swap.tsx
@@ -1,11 +1,14 @@
 import BasicLayout from "components/Layout/Basic";
+import { SwapProvider } from "context/swap";
 import type { NextPage } from "next";
 import Swap from "views/swap/Swap";
 
 const AggregatorPage: NextPage = () => {
     return (
         <BasicLayout>
-            <Swap/>
+            <SwapProvider>
+                <Swap />
+            </SwapProvider>
         </BasicLayout>
     );
 };
