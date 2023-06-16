4b2b03f213816aa9e4ffb988d825c38240cf4424
otis <luuquanghao2412@gmail.com>
2023-06-17 01:44:53 +0700

    feat: fallback ag tokens in storage

--- views/swap/Aggregator/hooks/useAgTokensBalance.tsx
+++ views/swap/Aggregator/hooks/useAgTokensBalance.tsx
@@ -12,6 +12,8 @@ import { useRecoilValue } from "recoil";
 import useSWR, { SWRConfiguration } from "swr";
 import { useDebounce } from "use-debounce";
 import useAgTokenList from "./useAgTokenList";
+import storage from "helper/storage";
+const emptyArray: any[] = [];
 
 const useAgTokensBalance = (swrConfig: SWRConfiguration = {}) => {
     const networkConfig: AccountInfoSliceNetworkType = useGetNetworkConfig().network;
@@ -23,29 +25,41 @@ const useAgTokensBalance = (swrConfig: SWRConfiguration = {}) => {
     const balanceFetchKey = useMemo(() => {
         return address ? [`${networkConfig.apiAddress}/accounts/${address}/tokens?identifiers=${ids}`, debounceLastTxHash]: null
     }, [address, debounceLastTxHash, ids, networkConfig.apiAddress]);
-    const {data: balances} = useSWR<Array<{balance: string, identifier: string}>>(balanceFetchKey, (url) => fetcher(url), {dedupingInterval: 6000, ...swrConfig, fallbackData: []}); 
-    const {data: tokens} = useSWR(`${networkConfig.apiAddress}/tokens?identifiers=${ids}&size=50`, fetcher, {fallbackData: []});
+    const {data: balances} = useSWR<Array<{balance: string, identifier: string}>>(balanceFetchKey, (url) => fetcher(url), {dedupingInterval: 6000, ...swrConfig, fallbackData: emptyArray}); 
+    const {data: tokens} = useSWR(`${networkConfig.apiAddress}/tokens?identifiers=${ids}`, (url) => fetcher(url).then(apiTokens => {
+        let result: IESDTInfo[] = [];
+        if (Array.isArray(apiTokens) && apiTokens.length > 0) {
+            result = apiTokens.map((t: any) => {
+                const staticESDT = TOKENS_MAP[t.identifier];
+                const esdt: IESDTInfo = {
+                    chainId:
+                        ENVIRONMENT.NETWORK === "mainnet"
+                            ? ChainId.Mainnet
+                            : ChainId.Devnet,
+                    decimals: t.decimals,
+                    identifier: t.identifier,
+                    name: staticESDT?.name || t.name,
+                    symbol: staticESDT?.symbol || t.ticker,
+                    logoURI: staticESDT?.logoURI || t.assets?.pngUrl || t.assets?.svgUrl,
+                    projectLink: t.assets?.website,
+                };
+                return esdt;
+            });
+            storage.local.setItem({key: "agTokens", data: result});
+        } else {
+            result = storage.local.getItem("agTokens") as IESDTInfo[] || []
+        }
+        return result;
+    }), {fallbackData: emptyArray, revalidateOnFocus: false});
+    
     const list = useMemo(() => {
         const map = Object.fromEntries((balances || []).map(b => [b.identifier, b]));
-        const tokensAmount: TokenAmount[] = (tokens || []).map((t: any) => {
-            const staticESDT = TOKENS_MAP[t.identifier];
-            const esdt: IESDTInfo = {
-                chainId:
-                    ENVIRONMENT.NETWORK === "mainnet"
-                        ? ChainId.Mainnet
-                        : ChainId.Devnet,
-                decimals: t.decimals,
-                identifier: t.identifier,
-                name: staticESDT?.name || t.name,
-                symbol: staticESDT?.symbol || t.ticker,
-                logoURI: staticESDT?.logoURI || t.assets?.pngUrl || t.assets?.svgUrl,
-                projectLink: t.assets?.website,
-            };
+        const tokensAmount: TokenAmount[] = (tokens || []).map((esdt) => {
             return new TokenAmount(esdt, map[esdt.identifier]?.balance || "0");
         });
-        
         return tokensAmount;
     }, [tokens, balances]);
+
     return list;
 }
 
