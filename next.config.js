67b114c0c6a78694bf315cdcebd90f2116a481a9
otis <luuquanghao2412@gmail.com>
2023-07-02 11:14:55 +0700

    feat: enable all feature mainnet

--- next.config.js
+++ next.config.js
@@ -34,7 +34,7 @@ const config = {
         const ignorePathsBase = ["/stake", "/stake/mint"];
         const ignorePaths =
             process.env.NEXT_PUBLIC_NETWORK === "mainnet"
-                ? ["/gov/bribe"]
+                ? []
                 : process.env.NEXT_PUBLIC_ASH_ENV === "alpha"
                 ? []
                 : ["/ashpoint"];
