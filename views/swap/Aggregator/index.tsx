4f53596dd2298c5f9909d570a2ae959bff068290
otis <luuquanghao2412@gmail.com>
2023-10-05 16:16:08 +0700

    feat: update ag req

--- views/swap/Aggregator/index.tsx
+++ views/swap/Aggregator/index.tsx
@@ -67,7 +67,7 @@ const Aggregator = memo(function Aggregator() {
     const [amountIn, setAmountIn] = useRecoilState(agAmountInAtom);
     const [cgkPriceChangeP, setCgkPriceChangeP] = useState(0);
     const amtIn = useMemo(
-        () => toWei(tokenIn, amountIn).idiv(1).toString(),
+        () => toWei(tokenIn, amountIn).idiv(1).toString(10),
         [tokenIn, amountIn]
     );
     // sor routing
