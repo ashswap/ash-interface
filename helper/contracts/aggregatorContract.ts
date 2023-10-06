fb6155de51e3524d0e6dfb55aa2b1e043bc05ef7
otis <luuquanghao2412@gmail.com>
2023-10-06 18:56:03 +0700

    feat: ag v1.2

--- helper/contracts/aggregatorContract.ts
+++ helper/contracts/aggregatorContract.ts
@@ -1,8 +1,9 @@
-import { Address, TokenTransfer } from "@multiversx/sdk-core/out";
+import { Address, Interaction, TokenTransfer } from "@multiversx/sdk-core/out";
 import { getAddress } from "@multiversx/sdk-dapp/utils";
 import aggregatorAbi from "assets/abi/aggregator.abi.json";
 import BigNumber from "bignumber.js";
 import Contract from "./contract";
+export const AG_MAX_FEE_PERCENT = 100_000;
 export type AggregatorStep = {
     token_in: string;
     token_out: string;
@@ -17,14 +18,55 @@ class AggregatorContract extends Contract<typeof aggregatorAbi> {
         super(address, aggregatorAbi);
     }
 
+    private isEgld(id: string) {
+        return id === "EGLD";
+    }
+
     async aggregate(
-        tokenPayments: TokenTransfer[],
+        payments: TokenTransfer[],
         steps: AggregatorStep[],
         limits: { token: string; amount: BigNumber }[]
     ) {
-        let interaction = this.contract.methods.aggregate([steps, ...limits]);
+        const interaction = this.contract.methods.aggregate([steps, ...limits]);
         interaction
-            .withMultiESDTNFTTransfer(tokenPayments)
+            .withMultiESDTNFTTransfer(payments)
+            .withSender(new Address(await getAddress()));
+        interaction.withGasLimit(20_000_000 + steps.length * 15_000_000);
+        return this.interceptInteraction(interaction);
+    }
+
+    async _aggregateEgld(
+        egldAmount: BigNumber.Value,
+        steps: AggregatorStep[],
+        limits: { token: string; amount: BigNumber }[],
+        protocol?: string
+    ) {
+        const params: any[] = [steps, limits];
+        if (protocol && protocol !== Address.Zero().bech32()) {
+            params.push(protocol);
+        }
+        const interaction = this.contract.methods.aggregateEgld(params);
+        interaction
+            .withValue(TokenTransfer.egldFromBigInteger(egldAmount))
+            .withSender(new Address(await getAddress()));
+        interaction.withGasLimit(20_000_000 + steps.length * 15_000_000);
+        return this.interceptInteraction(interaction);
+    }
+
+    async _aggregateEsdt(
+        esdtPayment: TokenTransfer,
+        steps: AggregatorStep[],
+        limits: { token: string; amount: BigNumber }[],
+        returnEgld: boolean,
+        protocol?: string
+    ) {
+        const params: any[] = [steps, limits, returnEgld];
+        if (protocol && protocol !== Address.Zero().bech32()) {
+            params.push(protocol);
+        }
+        const interaction = this.contract.methods.aggregateEsdt(params);
+        interaction
+            .withSingleESDTTransfer(esdtPayment)
             .withSender(new Address(await getAddress()));
         interaction.withGasLimit(20_000_000 + steps.length * 15_000_000);
         return this.interceptInteraction(interaction);
